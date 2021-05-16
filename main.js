f7fe9ee4f975b3dec3756d68f1622f5961674146
Alexander Eklöw <aleek874@student.liu.se>
2021-05-16 17:11:22 +0200

    Co-authored-by: Gustaf Norberg <gusno115@student.liu.se>

--- main.js
+++ main.js
@@ -1,24 +1,4 @@
 //Array med alla namn på flikarna 
-// let sections = ["about", "projects", "members", "contact"]
-
-//Funktionen visar texten tillhörande knappen användaren just klickade på och gömmer alla andra textrutor
-// function ChangeSection(clicked_section){
-//     for (i = 0; i < sections.length; i++)  {
-//         console.log(sections[i])
-//         if (clicked_section == sections[i]){
-//             document.getElementById(sections[i]).style.display = "block"
-//         }
-//         else {
-//             document.getElementById(sections[i]).style.display = "none"
-//         }
-//     }
-// }
-
-// ChangeSection("about")
-
-
-
-//Array med alla namn på flikarna 
 let sections = ["about", "projects", "members", "contact"]
 
 //Funktionen visar texten tillhörande knappen användaren just klickade på och gömmer alla andra textrutor
