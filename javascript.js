a4af958385c34b3a43fc9891092a5f5753cd00e8
aleek874 <aleek874@student.liu.se>
2022-01-19 18:57:00 +0100

    Co-authored-by: Gustaf Norberg <gusno115@student.liu.se> Co-authored-by: Björn Lindqvist <bjoli469@student.liu.se> Co-authored-by: Johannes Fagerlind <johfa811@student.liu.se> Co-authored-by: Matilda Zackrisson <matza637@student.liu.se> Co-authored-by: Fanny Eriksson <faner383@student.liu.se> Co-authored-by: Marius Martin <marma935@student.liu.se>

--- javascript.js
+++ javascript.js
@@ -1,13 +1,17 @@
 window.onscroll = function() {scrollFunction()};
+// window.onscroll = function (e) {
+//   console.log(window.scrollY); // Value of scroll Y in px
+// };
 
 function scrollFunction() {
+  console.log(window.scrollY); // Value of scroll Y in px
   if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
     document.getElementById("header").style.height = "70px";
-    document.getElementById("header").style.fontSize = "10px";
+    document.getElementById("logo").style.height = "60px";
     document.getElementById("header").style.opacity = 0.9;
   } else {
     document.getElementById("header").style.height = "120px";
-    document.getElementById("header").style.fontSize = "18px";
+    document.getElementById("logo").style.height = "110px";
     document.getElementById("header").style.opacity = 1;
   }
 }
