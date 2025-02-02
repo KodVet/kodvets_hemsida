da626ae051bed8afc745823bdfa9dbdf647d1f49
cascading-jox <cascading.jox@gmail.com>
2025-02-02 14:42:37 +0100

    feat: HMR on static file change

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,24 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, type HmrContext } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit(), staticHMR()]
 });
+
+function staticHMR() {
+	return {
+		name: 'static-hmr',
+		enforce: 'post' as const,
+		handleHotUpdate(hmrContext: HmrContext): void {
+			// match any file in the static directory
+			if (hmrContext.file.match('static/.*$')) {
+				hmrContext.server.ws.send({
+					type: 'full-reload',
+					path: '*'
+				});
+			}
+		}
+	};
+}
+
+
