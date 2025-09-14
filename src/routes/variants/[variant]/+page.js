bfd1604196505fa4392903fac8e1ef36f3fcb39b
cascading-jox <cascading.jox@gmail.com>
2025-09-14 18:21:29 +0200

    fix: build error & update deps

---
+++ src/routes/variants/[variant]/+page.js
@@ -0,0 +1,11 @@
+export const prerender = true;
+
+export function entries() {
+	return [
+		{ variant: 'joachim-24-25' },
+		{ variant: 'fredrik-24-25' },
+		{ variant: 'bella-24-25' },
+		{ variant: 'ellen-24-25' },
+		{ variant: 'kodvet-21-22' }
+	];
+}
