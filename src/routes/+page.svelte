fa39d0fd96d232387f2f680f2c931a39bcce9495
cascading-jox <cascading.jox@gmail.com>
2025-09-21 15:23:43 +0200

    fix: image link

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,7 +12,7 @@
 	<meta property="og:locale" content="sv" />
 	<meta property="og:description" content="KodVet är ett utskott till sektionen för kognitionsvetenskap på LiU och filfaks enda programmeringsutskott!" />
 	<meta property="og:url" content="https://kodvet.kogvet.se" />
-	<meta property="og:image" content="images/kodvet_logo_rund_indigo_bla.png" />
+	<meta property="og:image" content="/images/kodvet_logo_rund_indigo_bla.png" />
 </svelte:head>
 
 <Hero></Hero>
