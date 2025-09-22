f4fd69b36986fd5832334d2bb3f7cf2c37175900
ellenpalmqvist <ellenpalmqvist04@gmail.com>
2025-09-22 15:36:26 +0200

    JSON Ld

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,6 +13,51 @@
 	<meta property="og:description" content="KodVet är ett utskott till sektionen för kognitionsvetenskap på LiU och filfaks enda programmeringsutskott!" />
 	<meta property="og:url" content="https://kodvet.kogvet.se" />
 	<meta property="og:image" content="/images/kodvet_logo_rund_indigo_bla.png" />
+
+	<script type="application/ld+json">
+	{ 
+		"@context" :  "https://schema.org",
+		"@type" : "BreadcrumbList", 
+		"itemListElement": [
+			{
+				"@type" : "ListItem", 
+				"@position" : 1, 
+				"name": "Hem",
+				"item": "https://kodvet.kogvet.se"
+			}, 
+			{
+				"@type" : "ListItem", 
+				"@position" : 2, 
+				"name": "Joachim 24/25",
+				"item": "https://kodvet.kogvet.se/variants/joachim-24-25"
+			}, 
+			{
+				"@type" : "ListItem", 
+				"@position" : 3, 
+				"name": "Fredrik 24/25",
+				"item": "https://kodvet.kogvet.se/variants/fredrik-24-25"
+			}, 
+			{
+				"@type" : "ListItem", 
+				"@position" : 4, 
+				"name": "Bella 24/25",
+				"item": "https://kodvet.kogvet.se/variants/bella-24-25"
+			}, 
+			{
+				"@type" : "ListItem", 
+				"@position" : 5, 
+				"name": "Ellen 24/25",
+				"item": "https://kodvet.kogvet.se/variants/ellen-24-25"
+			}, 
+			{
+				"@type" : "ListItem", 
+				"@position" : 6, 
+				"name": "kodVet 21/22",
+				"item": "https://kodvet.kogvet.se/variants/kodvet-21-22"
+			}
+		]
+	}
+	</sript>
 </svelte:head>
 
 <Hero></Hero>
